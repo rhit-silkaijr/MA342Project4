30674cb4d3b276cb6f34596a02228fa6af78d91c
Luke Gaston <lgaston@myhoosierneighbors.com>
2026-04-30 10:31:42 -0400

    Fixed shit

--- StocksTextbookExample.m
+++ StocksTextbookExample.m
@@ -2,3 +2,26 @@
 clear variables;
 close all;
 clc;
+
+% Pull at opening prices
+[dateRange, stockIndex, P] = DataProcessing(2);
+
+n = size(P, 2);
+
+% Get stock returns
+R = (P(2:end, :) - P(1:(end-1), :)) ./ P(1:(end-1), :);
+
+% Mean returns
+r = mean(R);
+
+% Covariance matrix
+C = cov(R);
+
+% Risk factor
+alpha = 0.50;
+
+% optimization, no more investment than 20% into any stock
+inv_limit = 0.20;
+[w, optVal] = quadprog((1-alpha)*2*C, -alpha*r, [], [], ones(1,n), [1], zeros(n,1), inv_limit*ones(n,1));
+
+
